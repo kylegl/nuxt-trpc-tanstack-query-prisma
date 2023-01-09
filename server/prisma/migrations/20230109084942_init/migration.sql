c5f18ffe114e57900350ace642b1e5904c098787
Kyle <lauber.kyle@gmail.com>
2023-01-09 01:40:48 -0800

    chore: added supabase postgres db

--- server/prisma/migrations/20230109084942_init/migration.sql
+++ server/prisma/migrations/20230109084942_init/migration.sql
@@ -1,11 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
+    "username" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
