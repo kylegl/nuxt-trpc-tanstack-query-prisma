f9c23eac8f7e1d4b033f821450ec8219907c06db
Kyle <lauber.kyle@gmail.com>
2023-01-13 01:16:22 -0800

    chore: modified prisma client

--- server/prisma/prisma.ts
+++ server/prisma/prisma.ts
@@ -1,14 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import { config } from 'dotenv'
 
-declare global {
-  // eslint-disable-next-line vars-on-top, no-var
-  var prisma: PrismaClient | undefined
-}
+export const prisma = new PrismaClient()
 
-config()
-
-export const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production')
-  global.prisma = prisma
