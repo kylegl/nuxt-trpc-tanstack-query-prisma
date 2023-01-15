c8c4de1425b039c33203a5bd0524acb6f8316b1a
Kyle <lauber.kyle@gmail.com>
2023-01-14 23:54:37 -0800

    chore: changed trpc server url to path, loaded env vars to prisma client with dotenv

--- plugins/client.ts
+++ plugins/client.ts
@@ -2,9 +2,7 @@ import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
 import type { AppRouter } from '~~/server/trpc/routers/appRouter'
 
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-
-  const url = `/api/trpc`
+  const url = '/api/trpc'
   const client = createTRPCNuxtClient<AppRouter>({
     links: [
       httpBatchLink({
